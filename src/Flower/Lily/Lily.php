b988237dbc9862b342e8721602259390e9855555
bailefei <bailefei@local.dev>
2018-04-27 15:14:48 +0800

    提交

---
+++ src/Flower/Lily/Lily.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/27
+ * Time: 14:32
+ */
+
+namespace Flower\Lily;
+
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
---
+++ src/Flower/Rose/Rose.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/27
+ * Time: 14:32
+ */
+
+namespace Flower\Rose;
+
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
